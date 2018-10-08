a9b48f644ea57b1a4cb581b43d30fcbfa7eead2b
quyenphamkhac <pkquyen@sk-global.biz>
2018-10-08 16:06:05 +0700

    First commit

---
+++ src/containers/MainApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainApp extends Component {
+    render() {
+        return (
+            <div>
+                Hello Flickr
+            </div>
+        );
+    }
+}
+
+export default MainApp;
